b0fed605ff560fd09cfea7a021a52ab1ae70f7e7
Florian Sattler <vuld3r@gmail.com>
2023-07-06 20:51:18 +0200

    Ports helper functions over from FeaturePerfCSCollection

---
+++ include/fp_util/feature_cmd.h
@@ -0,0 +1,37 @@
+#ifndef FP_UTIL_FEATURECMD_H
+#define FP_UTIL_FEATURECMD_H
+
+#include <exception>
+#include <stdlib.h>
+#include <string>
+
+namespace fpcsc {
+inline bool isFeatureEnabled(int argc, char *argv[], std::string FeatureName) {
+  for (int CurrentArg = 1; CurrentArg < argc; ++CurrentArg) {
+    if (argv[CurrentArg] == FeatureName) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+inline long getFeatureValue(int argc, char *argv[], std::string FeatureName) {
+  int CurrentArg = 1;
+  for (; CurrentArg < argc; ++CurrentArg) {
+    if (argv[CurrentArg] == FeatureName) {
+      ++CurrentArg;
+      break;
+    }
+  }
+
+  if (CurrentArg >= argc) {
+    return 0;
+  }
+
+  return strtol(argv[CurrentArg], NULL, 0);
+}
+
+} // namespace fpcsc
+
+#endif // FP_UTIL_FEATURECMD_H
