8e1b2e355f924a1e2ba5e4e2c756e5a1f7e51271
Lukas <l.abelt@luabelt.de>
2023-11-18 18:31:25 +0100

    - Added detect function to FeaturePerfUtility

---
+++ include/fp_util/feature_detect.h
@@ -0,0 +1,18 @@
+/**
+* Author:   Lukas 
+* Created:  18/11/2023.
+**/
+
+#ifndef FEATURE_DETECT_H
+#define FEATURE_DETECT_H
+
+namespace fp_util {
+
+	__attribute__((feature_variable("__VARA__DETECT__"))) void detect() {
+		long counter = 0;
+		asm volatile("" : "+g"(counter) : :);
+		counter++;
+	}
+}
+
+#endif //FEATURE_DETECT_H
