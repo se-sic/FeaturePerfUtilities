a7e8c04962e9e6c416f5de9fa6e8837547fbb0b2
Lukas Abelt <github@login.luabelt.de>
2023-11-19 13:16:57 +0100

    Fixed formatting

--- include/fp_util/feature_detect.h
+++ include/fp_util/feature_detect.h
@@ -1,18 +1,18 @@
 /**
-* Author:   Lukas 
-* Created:  18/11/2023.
-**/
+ * Author:   Lukas
+ * Created:  18/11/2023.
+ **/
 
 #ifndef FEATURE_DETECT_H
 #define FEATURE_DETECT_H
 
 namespace fp_util {
 
-	__attribute__((feature_variable("__VARA__DETECT__"))) void detect() {
-		long counter = 0;
-		asm volatile("" : "+g"(counter) : :);
-		counter++;
-	}
+__attribute__((feature_variable("__VARA__DETECT__"))) void detect() {
+  long counter = 0;
+  asm volatile("" : "+g"(counter) : :);
+  counter++;
 }
+} // namespace fp_util
 
-#endif //FEATURE_DETECT_H
+#endif // FEATURE_DETECT_H
